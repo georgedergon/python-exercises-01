74b7950bf092bea130e29be0c9ddfd1ac3c0f8c9
george$ <georgedergon@gmx.de>
2024-10-01 17:30:17 +0200

    exercise_02.py

---
+++ exercise_02.py
@@ -0,0 +1,16 @@
+a = 10
+b = 3
+
+Summe = a + b
+differenz = a - b
+produkt = a * b
+ganzzahliger_quotient = a // b
+rest = a % b
+hoch = a ** b
+
+print("Summe:", summe)
+print("Differenz:", differenz)
+print("Produkt:", produkt)
+print("Ganzzahliger Quotient:", ganzzahliger_quotient)
+print("Rest der Division:", rest)
+print("a hoch b:", hoch)
