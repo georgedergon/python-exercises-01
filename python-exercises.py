275ae600561507a77bc97bf9f8fab4b1dfc22a86
george$ <georgedergon@gmx.de>
2024-10-01 16:33:10 +0200

    python-exercises

---
+++ python-exercises.py
@@ -0,0 +1,11 @@
+# das ist mein kommentar
+first_name= "George"
+last_name = "Dergon"
+birth_year = 1988
+has_license = True
+
+print("Name:", first_name )
+print("Name:", last_name)
+print("gebursjahr:", birth_year)
+print(has_license)
+
